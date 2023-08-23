ae0041577f39c46e35b00e513c611cc427f5e4e8
nfroidure <nicolas.froidure@gmail.com>
2023-08-23 13:04:12 +0000

    Deploying to gh-pages from @ nfroidure/tribunator@34db365606e2c2937276f5b829287b6b00800975 🚀

---
+++ _next/static/Vs9oO2lwxcq1OzjAzYx9h/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002F[...slug]","\u002Felu-es","\u002Felu-es\u002F[id]","\u002Felu-es\u002Fpages\u002F[page]","\u002Fgroupes","\u002Fgroupes\u002F[id]","\u002Fgroupes\u002Fpages\u002F[page]","\u002Ftribunes","\u002Ftribunes\u002F[id]","\u002Ftribunes\u002Fpages\u002F[page]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
